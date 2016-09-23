1cc8a6550d739045f2983e2ae9a67622f011e82d
Sergey Poluyan <spoluyan@gmail.com>
2016-09-23 15:20:48 +0300

    Search method

--- src/main/java/pw/spn/crawler/livelib/LivelibCrawler.java
+++ src/main/java/pw/spn/crawler/livelib/LivelibCrawler.java
@@ -1,5 +1,7 @@
 package pw.spn.crawler.livelib;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -7,13 +9,16 @@ import java.util.Optional;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import pw.spn.crawler.livelib.entity.Book;
+import pw.spn.crawler.livelib.exception.LivelibCrawlerException;
 import pw.spn.crawler.livelib.http.LivelibHttpService;
 
 public class LivelibCrawler {
+    private static final String DEFAULT_ENCODING = "UTF-8";
     private static final String WISH_URL_TEMPLATE = "/userbooks?page=wishonly&login=%s&count=" + Integer.MAX_VALUE;
     private static final String READING_URL_TEMPLATE = "/userbooks?page=reading&login=%s&count=" + Integer.MAX_VALUE;
     private static final String UNREAD_URL_TEMPLATE = "/userbooks?page=unread&login=%s&count=" + Integer.MAX_VALUE;
     private static final String READ_URL_TEMPLATE = "/userbooks?page=read&login=%s&count=" + Integer.MAX_VALUE;
+    private static final String SEARCH_URL_TEMPLATE = "/booksearch?sort_order=rating&searchstring=%s&start=%d";
     private final LivelibHttpService httpService;
 
     public LivelibCrawler() {
@@ -47,6 +52,19 @@ public class LivelibCrawler {
 
     public List<Book> getUnread(String userName) {
         Optional<List<Book>> result = httpService.doGet(String.format(UNREAD_URL_TEMPLATE, userName),
+                new TypeReference<List<Book>>() {
+        });
+        return result.orElse(Collections.emptyList());
+    }
+
+    public List<Book> searchForBook(String query) {
+        String encodedSearchString;
+        try {
+            encodedSearchString = URLEncoder.encode(query, DEFAULT_ENCODING);
+        } catch (UnsupportedEncodingException e) {
+            throw new LivelibCrawlerException(e);
+        }
+        Optional<List<Book>> result = httpService.doGet(String.format(SEARCH_URL_TEMPLATE, encodedSearchString, 0),
                 new TypeReference<List<Book>>() {
         });
         return result.orElse(Collections.emptyList());
